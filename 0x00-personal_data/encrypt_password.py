58b6f4ec8d1dc517a3983d965462bb7dc36c598e
Sarah Gad <sarahgad298@gmail.com>
2024-09-01 08:46:54 +0300

    edit

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,10 +5,10 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """This fucntion retunns a hashed password"""
-    hashed = bcrypt.hashpw(b'password', bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """This fucntion checks if the password is valid or not"""
-    return bcrypt.checkpw(b'password', hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
