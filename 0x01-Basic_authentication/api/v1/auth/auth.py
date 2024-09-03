6c8db293e520be43e59fb81f8294aad5662d5b87
Sarah Gad <sarahgad298@gmail.com>
2024-09-04 00:38:47 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,15 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method 1"""
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None:
+            return True
+        else:
+            if path in excluded_paths or path + "/" in excluded_paths:
+                return False
+            else:
+                return True
 
     def authorization_header(self, request=None) -> str:
         """Method 2"""
