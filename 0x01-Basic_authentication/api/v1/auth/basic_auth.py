9be6feddb55ba56cb5e3a0dfcf84bca12961c338
Sarah Gad <sarahgad298@gmail.com>
2024-09-04 03:13:21 +0300

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,17 @@ class BasicAuth(Auth):
     """This is a child class that inherit from Auth class"""
     def __init__(self) -> None:
         super().__init__()
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """this method will return the Base64 part"""
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith("Basic "):
+            return None
+        else:
+            first, second = authorization_header.split(" ")
+            return second
