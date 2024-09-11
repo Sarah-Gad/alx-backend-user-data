2e2a534d76426ce142f86f0b2d44a4b7cc3a5803
Sarah Gad <sarahgad298@gmail.com>
2024-09-11 08:53:48 +0300

    edit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> bytes:
     """This method return returns hasdhed password"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
