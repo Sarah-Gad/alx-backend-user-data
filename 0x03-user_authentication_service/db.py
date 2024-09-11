7debe2d1849b759934d5ce9319ec0178abdefdde
Sarah Gad <sarahgad298@gmail.com>
2024-09-11 07:15:10 +0300

    ediiiiit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-
+from sqlalchemy.orm.session import Session
 from user import Base, User
-
-
 class DB:
-    """DB class"""
-
+    """DB class.
+    """
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """Initialize a new DB instance.
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
-
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user"""
+        """Adds a new user to the database.
+        """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
@@ -40,18 +37,18 @@ class DB:
             self._session.rollback()
             new_user = None
         return new_user
-
     def find_user_by(self, **kwargs) -> User:
-        """Searches for a user in the DB"""
-        query = self._session.query(User)
+        """This method finds a user from the database"""
         try:
             for key, value in kwargs.items():
-                column = getattr(User, key, None)
-                if column:
-                    query = query.filter(column == value)
-                else:
+                if not hasattr(User, key):
                     raise InvalidRequestError
-            result = query.one()
-            return result
+                user = self._session.query(
+                    User).filter(
+                        getattr(User, key, None) == value).first()
+            if user is None:
+                raise NoResultFound
+            else:
+                return user
         except (InvalidRequestError, NoResultFound):
             raise
