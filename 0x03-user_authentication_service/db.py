151ce44f6dd8fedc9c96bbb35fdc9a368b8bfb93
Sarah Gad <sarahgad298@gmail.com>
2024-09-11 07:16:47 +0300

    85

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
+
 from user import Base, User
+
+
 class DB:
     """DB class.
     """
@@ -18,6 +21,7 @@ class DB:
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
+
     @property
     def _session(self) -> Session:
         """Memoized session object.
@@ -26,6 +30,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database.
         """
@@ -37,6 +42,7 @@ class DB:
             self._session.rollback()
             new_user = None
         return new_user
+
     def find_user_by(self, **kwargs) -> User:
         """This method finds a user from the database"""
         try:
