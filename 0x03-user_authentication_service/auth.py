eff8f9a9ab21e6c226e78c422ebd4ae8a8959864
Sarah Gad <sarahgad298@gmail.com>
2024-09-11 09:12:31 +0300

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 """This module defines a _hash_password method"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """This method return returns hasdhed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """this method will register a new useer"""
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            pass
+        hashed = _hash_password(password)
+        created_user = self._db.add_user(email, hashed)
+        return created_user
