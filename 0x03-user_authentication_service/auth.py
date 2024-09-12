8440dc0735c0460eedb7ddff01adaae8b50f59ed
Sarah Gad <sarahgad298@gmail.com>
2024-09-13 01:13:30 +0300

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,3 +65,9 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """This method destroys the session by
+        setting the session_id attribute to None"""
+        self._db.update_user(user_id, session_id=None)
+        return None
