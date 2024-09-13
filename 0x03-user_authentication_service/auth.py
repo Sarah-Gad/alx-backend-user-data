b37ed5de9f6fd20d113b74827fce032e1a91336b
Sarah Gad <sarahgad298@gmail.com>
2024-09-13 03:08:59 +0300

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,13 @@ class Auth:
         setting the session_id attribute to None"""
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """This method generates a reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_uuid = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_uuid)
+        return user.reset_token
