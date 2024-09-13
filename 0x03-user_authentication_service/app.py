3d89a611cd8ad1f5865b96196d76c9838cdd1818
Sarah Gad <sarahgad298@gmail.com>
2024-09-13 04:49:47 +0300

    edit the exception Error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,7 +91,7 @@ def update_password():
     new_password = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, new_password)
-    except NoResultFound:
+    except ValueError:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"}), 200
 
