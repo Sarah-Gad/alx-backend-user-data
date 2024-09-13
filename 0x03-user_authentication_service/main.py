0e5554cf499a2aba4a9f47346645ae1da8f6972e
Sarah Gad <sarahgad298@gmail.com>
2024-09-13 06:01:10 +0300

    pycodetyle

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,10 +8,14 @@ EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
 
+
 def register_user(EMAIL, PASSWD):
-    resonse = requests.post("http://localhost:5000/users", data={"email": EMAIL, "password": PASSWD})
+    resonse = requests.post(
+        "http://localhost:5000/users",
+        data={"email": EMAIL, "password": PASSWD})
     assert resonse.status_code == 200
     assert resonse.json() == {"email": EMAIL, "message": "user created"}
+
 
 if __name__ == "__main__":
     register_user(EMAIL, PASSWD)
